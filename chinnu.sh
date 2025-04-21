66b33d5f554b7e6b3e33b71500f067d3a00fc9d1
priya <shanthipriya00247@gmail.com>
2025-04-21 17:29:26 +0530

    priya

--- chinnu.sh
+++ chinnu.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
-        echo "enter number01:"
-	read a
-	echo "enter number02:"
-	read b
-	echo "enter number03:"
-	read c
-	echo "enter number04:"
-	read d
-if ((a>b && a>c && a>d));then
-    echo  "$a is the largest number:"
-elif ((b>c && b>d ));then
-	echo "$b is the largest number:"
-elif ((c>d));then
-	echo "$c is the largest number:"
-else
-	echo "$d is the largest number:"
-fi
+for ((i=1;i<=5;i++));
+do
+	echo "$i"
+done
 
